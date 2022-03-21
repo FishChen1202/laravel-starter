a27e6951067925f51b156631c3b9d5998b9513fb
Fish <fish.chen@onramplab.com>
2022-03-21 18:05:56 +0800

    chore(test): [phpunit] setup env

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Redis;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        /** @phpstan-ignore-next-line  */
+        Redis::flushDB();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        /** @phpstan-ignore-next-line  */
+        Redis::flushDB();
+    }
 }
